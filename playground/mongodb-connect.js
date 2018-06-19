151995409c9e1733f84e8a50daa8dad26e8f3061
Deathworld <corentin.grall@gmail.com>
2018-06-19 12:40:16 +0000

    Setup connection to a MongoDB Database and insert data into

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,13 +1,14 @@
-const MongoClient = require('mongodb').MongoClient;
+/* Create variables MongoClient and ObjectID, coming from mongodb package*/
+const {MongoClient, ObjectID} = require('mongodb');
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+
+MongoClient.connect('mongodb://localhost:27017/UsersList', (err, client) => {
     console.log('Connecting to MongoDB server');
     if(err){
         return console.log('Unable to connect to MongoDB server');
     }
     console.log('Connected to MongoDB server');
 
-    // Insert new doc into Users (name, age, location)
 
     const db = client.db('Users');
     db.collection('Users').insertOne({
@@ -19,7 +20,6 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
         if(err){
             return console.log('Unable to register User', err);
         }
-        console.log(JSON.stringify(result.ops, undefined, 2));
     });
 
 
