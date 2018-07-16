ab8f4e7081c5c25875510bf06dfb104f039fd994
Corentin Grall <corentin.grall@gmail.com>
2018-07-16 15:36:01 +0200

    Finished Hashing tests

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,31 +1,43 @@
 const {SHA256} = require('crypto-js');
-
-
-// Create Hash from message
-var message = 'I am user number 3';
-var hash = SHA256(message).toString();
-
-console.log(`Message: ${message}`);
-console.log(`Hash : ${hash}`);
-
-// Create Hash from object then compare it to another hash
+const jwt = require('jsonwebtoken');
 
 var data = {
-    id: 4
+    id: 10
 };
 
-var token = {
-    data,
-    hash: SHA256(JSON.stringify(data) + 'secretkey').toString()
-}
 
-token.data.id = 5;
-token.hash = SHA256(JSON.stringify(token.data)).toString;
+var token = jwt.sign(data, '123abc');
+console.log(token);
 
-var resultHash = SHA256(JSON.stringify(token.data) + 'secretkey').toString();
+var decoded = jwt.verify(token, '123abcc');
+console.log('decoded', decoded);
 
-if (resultHash === token.hash) {
-    console.log('Data was not changed');
-} else {
-    console.log('Data was changed. Do not trust!');
-}
+
+// // Create Hash from message
+// var message = 'I am user number 3';
+// var hash = SHA256(message).toString();
+
+// console.log(`Message: ${message}`);
+// console.log(`Hash : ${hash}`);
+
+// // Create Hash from an object then compare it to another hash
+
+// var data = {
+//     id: 4
+// };
+
+// var token = {
+//     data,
+//     hash: SHA256(JSON.stringify(data) + 'secretkey').toString()
+// }
+
+// token.data.id = 5;
+// token.hash = SHA256(JSON.stringify(token.data)).toString;
+
+// var resultHash = SHA256(JSON.stringify(token.data) + 'secretkey').toString();
+
+// if (resultHash === token.hash) {
+//     console.log('Data was not changed');
+// } else {
+//     console.log('Data was changed. Do not trust!');
+// }
